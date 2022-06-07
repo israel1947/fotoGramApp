5e1ac20468be86901bcb216f7c34c62e7548381c
israel1947 <israeldadidsdvsf@gmail.com>
2022-06-07 13:14:29 -0500

    The conection to db is implement

--- fotoGramServer/dist/index.js
+++ fotoGramServer/dist/index.js
@@ -5,9 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./class/server"));
 const users_1 = __importDefault(require("./routes/users"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const serve = new server_1.default();
 //routes of the aplication
 serve.app.use('/user', users_1.default);
+//conect db
+mongoose_1.default.connect('mongodb://localhost:27017/fotosgram', {
+    useNewUrlParser: true,
+}, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('DB IS ONLINE');
+});
 //express init
 serve.start(() => {
     try {
--- fotoGramServer/index.ts
+++ fotoGramServer/index.ts
@@ -1,10 +1,21 @@
 import Server from "./class/server";
 import userRoute from "./routes/users";
+import mongoose, { ConnectOptions } from "mongoose";
 
 const serve = new Server();
 
 //routes of the aplication
-serve.app.use('/user', userRoute)
+serve.app.use('/user', userRoute);
+
+//conect db
+mongoose.connect('mongodb://localhost:27017/fotosgram',{
+    useNewUrlParser:true,
+  } as ConnectOptions,(err)=>{
+      if(err){
+        throw err;
+      }
+    console.log('DB IS ONLINE');
+});
 
 //express init
 serve.start(()=>{
