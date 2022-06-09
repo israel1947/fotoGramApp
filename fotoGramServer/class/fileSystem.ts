a031181e0cbdb005d5bab8bf990b82f61073aada
israel1947 <israeldadidsdvsf@gmail.com>
2022-06-09 10:50:23 -0500

    The service to create unique name for file upload is implement

--- fotoGramServer/class/fileSystem.ts
+++ fotoGramServer/class/fileSystem.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { FileUpload } from "../interface/file-upload";
 import fs from "fs";
+import uniqid from "uniqid";
 
 export default class FileSystem{
     constructor(){};
@@ -8,13 +9,31 @@ export default class FileSystem{
     saveTempImage(file:FileUpload, userId:string){
         //image storage location
         const path = this.createFolderUser(userId);
+
+        //name of file
+        const nameFile = this.generateUniqueName(file.name);
+        console.log(file.name);
+        console.log(nameFile);
+        
+    }
+
+    private generateUniqueName(originalName:string){//origiunal name of image when is upload Ex:name.png
+        //extract extention of file
+        const nameArr=originalName.split('.')
+        const extention = nameArr[nameArr.length -1];
+
+        //create unique name
+        const uniqId = uniqid();
+
+        //return new name + unique id Ex:sfs5fs65fsf4.png
+        return `${uniqId}.${extention}`;
     }
 
     //create user folders 
     private createFolderUser(userId:string){
         const pathUser = path.resolve(__dirname,'../upload', userId);
         const pathUserTemp = pathUser + '/temp';
-        console.log(pathUser);
+        //console.log(pathUser);
         
 
         //verify if the folder temp exist
--- fotoGramServer/dist/class/fileSystem.js
+++ fotoGramServer/dist/class/fileSystem.js
@@ -5,18 +5,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
+const uniqid_1 = __importDefault(require("uniqid"));
 class FileSystem {
     constructor() { }
     ;
     saveTempImage(file, userId) {
         //image storage location
         const path = this.createFolderUser(userId);
+        //name of file
+        const nameFile = this.generateUniqueName(file.name);
+        console.log(file.name);
+        console.log(nameFile);
+    }
+    generateUniqueName(originalName) {
+        //extract extention of file
+        const nameArr = originalName.split('.');
+        const extention = nameArr[nameArr.length - 1];
+        //create unique name
+        const uniqId = uniqid_1.default();
+        //return new name + unique id Ex:sfs5fs65fsf4.png
+        return `${uniqId}.${extention}`;
     }
     //create user folders 
     createFolderUser(userId) {
         const pathUser = path_1.default.resolve(__dirname, '../upload', userId);
         const pathUserTemp = pathUser + '/temp';
-        console.log(pathUser);
+        //console.log(pathUser);
         //verify if the folder temp exist
         const exist = fs_1.default.existsSync(pathUser);
         if (!exist) {
