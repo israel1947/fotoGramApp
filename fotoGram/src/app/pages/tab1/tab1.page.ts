33f863966461ec67e7009688534021f9811a437a
israel1947 <israeldadidsdvsf@gmail.com>
2022-06-13 13:22:29 -0500

    The infinite scroll of post is implemented

--- fotoGram/src/app/pages/tab1/tab1.page.ts
+++ fotoGram/src/app/pages/tab1/tab1.page.ts
@@ -12,13 +12,25 @@ export class Tab1Page implements OnInit {
   posts:Post[]=[];
 
   constructor( private postService:PostsService) {}
+
   ngOnInit(): void {
+    this.loadMorePosts();
+  };
+  
+
+  loadMorePosts(event?){
     this.postService.getPost()
       .subscribe(resp =>{
         this.posts.push(...resp.posts);
-        console.log(resp);
-        
+        if(event){
+          event.target.complete();
+          //finish de process of infinite scrool
+         if(resp.posts.length === 0){
+          event.target.disabled=true
+        }
+      }
+        console.log(resp);   
     });
-  };
+  }
 
 }
