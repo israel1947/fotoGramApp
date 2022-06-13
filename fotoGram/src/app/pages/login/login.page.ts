fd46275ce542fbc24f63b52b41902f40af154ba1
israel1947 <israeldadidsdvsf@gmail.com>
2022-06-13 15:19:02 -0500

    The user interfaz in the page login is implement

--- fotoGram/src/app/pages/login/login.page.ts
+++ fotoGram/src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  avatars = [
+    {
+      img: 'av-1.png',
+      seleccionado: true
+    },
+    {
+      img: 'av-2.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-3.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-4.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-5.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-6.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-7.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-8.png',
+      seleccionado: false
+    },
+];
+
+slidesOptions = {
+  slidesPerView: 3.5,
+  
+};
+
+@ViewChild('slidePrincipal') slides:IonSlides;
+
   constructor() { }
 
-  ngOnInit() {
+   ngAfterViewInit() {
+    this.slides.lockSwipes(true)
+  }  
+  
+  ngOnInit() {}
+
+  swipeNext(){
+    this.slides.lockSwipes(false);
+    this.slides.slideNext();
+    this.slides.lockSwipes(true);
+  }
+  swipePrev(){
+    this.slides.lockSwipes(false);
+    this.slides.slidePrev();
+    this.slides.lockSwipes(true);
+
+  }
+
+  onSelectAvatar(avatar){
+    this.avatars.forEach(ava=>ava.seleccionado=false);
+    avatar.seleccionado = true;
+  }
+
+  login(fLogin:NgForm){
+    console.log(fLogin.valid);
+  }
+
+  register(fRegister:NgForm){
+    console.log(fRegister.valid);
   }
 
 }
